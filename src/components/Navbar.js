7bc7076cbef2251c223fe4554df453f84e9b819a
fidashn <87206839+fidashn@users.noreply.github.com>
2022-06-19 02:13:22 +0300

    created the navbar elements and style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+    <nav className='nav'>
+    <span class="name">Fida Shnakher</span>
+    <ul className='ul1'>
+      {/* <a href="FIDA--SHNAKHER-FE.pdf"><button class="download">Download cv</button></a> */}
+      <button class="download">Download cv</button>
+      <li>Home</li>
+      <li>Projects</li>
+    </ul>
+    </nav></div>
   )
 }
 
