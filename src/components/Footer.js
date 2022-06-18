dc541c7a074cbdc1efdbd214566ff0cc3582274b
fidashn <87206839+fidashn@users.noreply.github.com>
2022-06-19 02:25:36 +0300

    footer + its links are ready

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <div class="footer-inner"><span class="span">&copy;2022 by Fida Shnakher</span>
+        <div class="call"><i class="fas fa-phone"></i>
+          058-5141485
+        </div>
+        <div class="write"><i class="fas fa-envelope-open"></i>
+          fshnakher@gmail.com
+        </div>
+        <div class="follow">
+          <div class='icons'>
+            <a href="https://www.facebook.com/profile.php?id=100012226652201"><i class="fab fa-facebook fa-2x" ></i></a>
+            <a href="https://github.com/fidashn"><i class="fab fa-github fa-2x"></i></a>
+            <a href="https://www.linkedin.com/in/fida-shnakher-0b0b4a1b5/"><i class="fab fa-linkedin fa-2x"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
     <ul className='ul1'>
       {/* <a href="FIDA--SHNAKHER-FE.pdf"><button class="download">Download cv</button></a> */}
       <button class="download">Download cv</button>
-      <li>Home</li>
+      <li>About</li>
       <li>Projects</li>
     </ul>
     </nav></div>
